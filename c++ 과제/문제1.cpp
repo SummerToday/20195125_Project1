9c29e030ef1a57c1f865c799cb9bff7e3807744a
SummerToday <88650436+SummerToday@users.noreply.github.com>
2022-09-12 16:35:19 +0900

    continue

--- c++ 과제/문제1.cpp
+++ c++ 과제/문제1.cpp
@@ -9,8 +9,10 @@ int main() {
 	cout << "영문자를 입력하고 ctrl+z를 치세요" << endl;
 	while (1) {
 		cin >> c;
-		if ((int)c > 64 && (int)c < 91)
+		if ((int)c > 64 && (int)c < 91) {
 			cout << "Check to alphabet" << endl;
+			continue;
+		}
 		if (cin.eof()) break;
 		switch (c) {
 		case 'a':
